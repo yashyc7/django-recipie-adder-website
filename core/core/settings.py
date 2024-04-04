30dee24809f56ab54cc5c7e0172051daeabb0353
yashyc7 <yashc9411407582@gmail.com>
2024-04-05 00:08:34 +0530

    vercel support has been added

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$wb)k98g*mhdd(tsleovoo6a%unno66kpzf(f5783h*0wjc7ko'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
