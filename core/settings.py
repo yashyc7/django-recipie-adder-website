803f0b0b1d981ea905c55fd6fe0c7e1a23747d15
yashyc7 <yashc9411407582@gmail.com>
2024-04-10 23:24:52 +0530

    settings.py

--- core/settings.py
+++ core/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'USER':'postgres',
         'PASSWORD':'vUmxnPrfJNlbFZNyIPDErwliDbKUdasD',
